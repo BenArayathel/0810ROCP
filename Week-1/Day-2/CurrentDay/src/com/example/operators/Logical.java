c2ebe8d1ab628b3648cae49baddfc097b8e4f931
Ben Arayathel <ben.arayathel@gmail.com>
2020-08-11 12:55:45 -0400

    day 2, morning examples

---
+++ Week-1/Day-2/CurrentDay/src/com/example/operators/Logical.java
@@ -0,0 +1,59 @@
+package com.example.operators;
+
+public class Logical {
+	
+	public static void main(String[] args) {
+		
+		//Java Logical Operators
+		
+		
+		/*
+		 * if(*boolean expression*){
+		 * 	if(*boolean expression*){
+		 * 		//my logic
+		 * 		}
+		 * 
+		 * if(*boolean*){}
+		 * else if(*boolean*){}
+		 * 
+		 * Logical Operators 
+		 * && : returns true if both statements are true
+		 * || : returns true if either of the statements are true
+		 * !  : returns the opposite value
+		 * 
+		 * Bitwise Operators
+		 * &
+		 * |
+		 * 
+		 */
+		
+	
+		int a = 1;
+		int b = 7;
+		
+//		if(a != 1) {
+//			System.out.println("condition has been met");
+//		} else {
+//			System.out.println("condtion has not been met");
+//		}
+		
+//		System.out.println(a);
+		
+		
+		int x = 4 | 3;
+		
+		System.out.println(x);
+				
+//		if(a == 2 && b == 7 && (x = 7 & 3) ){
+//			System.out.println("values match");
+//		} else if(a == 1 || b == 7) {
+//			System.out.println("one of the values match");
+//		} else {
+//			System.out.println("values don't match");
+//		}
+		
+		
+		
+	}
+
+}
