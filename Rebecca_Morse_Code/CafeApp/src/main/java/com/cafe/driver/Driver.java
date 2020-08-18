69e72e8b1df455dc93db75bc264c4a9114534ec9
Rebecca Morse <becca.morse@gmail.com>
2020-08-18 09:13:23 -0400

    add functionality to add items menu

--- Rebecca_Morse_Code/CafeApp/src/main/java/com/cafe/driver/Driver.java
+++ Rebecca_Morse_Code/CafeApp/src/main/java/com/cafe/driver/Driver.java
@@ -10,7 +10,7 @@ import com.cafe.service.Menu;
 import com.cafe.service.Order;
 
 public class Driver {
-	
+
 	public static final String WELCOME_MESSAGE = "Welcome to Cat Cafe! To place an order, Enter your Name:";
 	public static final String GREETING = "Hello, ";
 	public static final String ORDER_PROMPT = "What would you like to order?";
@@ -21,52 +21,77 @@ public class Driver {
 	public static final String CONTINUE_ORDER_PROMPT = "Enter \n(A) To Add Items \n(R) To Remove Items \n(C) To Check out.";
 	public static final String ADD_ITEMS_PROMPT = "What would you like to add to your order?";
 	public static final String REMOVE_ITEMS_PROMPT = "What would you like to remove from your order?";
-	public static final String ORDER_TOTAL = "Total for order:";
+	public static final String ORDER_TOTAL_PREFIX = "Total for order:";
 	public static final String FINALIZE_ORDER_PROMPT = "Enter (U) To Update or change your order \nEnter (P) To Pay and complete your order.";
 	public static final String THANK_YOU = "Thank you for visiting the Cat Cafe, Come again soon!";
+	public static final String ENTER_VALID_OPTION = "Please enter a valid option";
 
 	private static String action;
 	static int currentOrderNumber = 1;
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		communicate(WELCOME_MESSAGE);
-				
+
 		Customer customer = establishCustomer(takeInput(sc));
-		
+
 		Menu menu = makeMenu();
-		//currently have this in present menu, but need to access menu in other location. 
-		
+
+		presentMenuAndAddToOrder(sc, customer, menu);
+		boolean stillOrdering = true;
+		while (stillOrdering) {
+			String selection = takeInput(sc);
+			switch (selection.toUpperCase()) {
+			case "A":
+				presentMenuAndAddToOrder(sc, customer, menu);
+				break;
+			case "R":
+				break;
+			case "C": stillOrdering = false;
+				break;
+			default:
+				communicate(ENTER_VALID_OPTION);
+				break;
+			}
+		}
+		System.out.println("Oops! I haven't written any more program yet.");
+
+		// each method - will change state or return??
+
+		sc.close();
+
+	}
+
+	private static void presentMenuAndAddToOrder(Scanner sc, Customer customer, Menu menu) {
 		presentMenu(customer);
-				
+
 		int itemKey = Integer.parseInt(takeInput(sc));
 		FoodItem item = menu.getMenu().get(itemKey);
-		
+
 		communicate(QUANTITY_PROMPT);
 		int quantity = Integer.parseInt(takeInput(sc));
-		
-		addItemToOrder(customer.getOrder(), item , quantity);
-		
+
+		addItemToOrder(customer.getOrder(), item, quantity);
+
 		displayOrder(customer.getOrder());
-		
-		System.out.println("Oops! I haven't written any more program yet.");
-		
-		
-		// each method - will change state or return??
-		
-		sc.close();
-		
+
+		promptforNextStep();
+	}
+
+	private static void promptforNextStep() {
+		communicate(CONTINUE_ORDER_PROMPT);
+
 	}
 
 	private static void displayOrder(Order order) {
 		communicate(DISPLAY_ORDER_TITLE);
-		for (Map.Entry<FoodItem, Integer> entry: order.getOrderContents().entrySet()) {
-			System.out.println(entry.getValue() + " " +  entry.getKey().getName() + "	$" + entry.getKey().getCost() + " (each)");
+		for (Map.Entry<FoodItem, Integer> entry : order.getOrderContents().entrySet()) {
+			communicate(
+					entry.getValue() + " " + entry.getKey().getName() + "	$" + entry.getKey().getCost() + " (each)");
 		}
-		communicate(CONTINUE_ORDER_PROMPT);
+		communicate(ORDER_TOTAL_PREFIX + "[insert order total here]");
 	}
 
 	private static void addItemToOrder(Order order, FoodItem item, int quantity) {
@@ -89,12 +114,12 @@ public class Driver {
 		String input = sc.nextLine();
 		return input;
 	}
-	
+
 	private static Customer establishCustomer(String takeInput) {
 		Customer customer = new Customer(takeInput, startOrder());
 		return customer;
 	}
-	
+
 	private static Order startOrder() {
 		Map newOrderMap = new HashMap<FoodItem, Integer>();
 		Order order = new Order(currentOrderNumber, newOrderMap);
@@ -102,19 +127,20 @@ public class Driver {
 		return order;
 	}
 
-	public static Menu makeMenu() { 
-		
+	public static Menu makeMenu() {
+
 		Map<Integer, FoodItem> menuContents = new HashMap<Integer, FoodItem>();
 
-		
 		FoodItem water = new FoodItem("Water", "Beverage", "Liquid Refreshment at its' finest!", 0);
 		FoodItem lemonade = new FoodItem("Lemonade", "Beverage", "The tastiest Summertime Drink", 2);
-		FoodItem veggieBurger = new FoodItem("Veggie Burger", "Entree","Our House-made Veggie Burgers, served with hand-cut fries.",10);
-		FoodItem pasta = new FoodItem("Pasta Supreme","Entree","House made pasta with the freshest Pesto around.",7);
-		FoodItem asparagus = new FoodItem("Asparagus Spears", "Side","Only the freshest!",17);
-		FoodItem cococadoShake = new FoodItem("Cococado Vegan Milkshake","Dessert", "Made with rice dream ice cream, bananas, coconut and avocado. You know you love it!",4);
-		FoodItem pie = new FoodItem("Pie","Dessert","Fresh seasonal pie.  Current variety: peach",5);
-		
+		FoodItem veggieBurger = new FoodItem("Veggie Burger", "Entree",
+				"Our House-made Veggie Burgers, served with hand-cut fries.", 10);
+		FoodItem pasta = new FoodItem("Pasta Supreme", "Entree", "House made pasta with the freshest Pesto around.", 7);
+		FoodItem asparagus = new FoodItem("Asparagus Spears", "Side", "Only the freshest!", 17);
+		FoodItem cococadoShake = new FoodItem("Cococado Vegan Milkshake", "Dessert",
+				"Made with rice dream ice cream, bananas, coconut and avocado. You know you love it!", 4);
+		FoodItem pie = new FoodItem("Pie", "Dessert", "Fresh seasonal pie.  Current variety: peach", 5);
+
 		menuContents.put(1, water);
 		menuContents.put(2, lemonade);
 		menuContents.put(3, veggieBurger);
@@ -122,20 +148,17 @@ public class Driver {
 		menuContents.put(5, asparagus);
 		menuContents.put(6, cococadoShake);
 		menuContents.put(7, pie);
-		
+
 		Menu menu = new Menu(menuContents);
-		
+
 		return menu;
 	}
-	
+
 	public static void displayMenu(Menu menu) {
-		for (Map.Entry<Integer, FoodItem> entry: menu.getMenu().entrySet()) {
-			System.out.println("(" + entry.getKey() + ") " + entry.getValue().getName() + " $" + entry.getValue().getCost());
+		for (Map.Entry<Integer, FoodItem> entry : menu.getMenu().entrySet()) {
+			System.out.println(
+					"(" + entry.getKey() + ") " + entry.getValue().getName() + " $" + entry.getValue().getCost());
 		}
 	}
-	
-	
-	
 
-	
 }
