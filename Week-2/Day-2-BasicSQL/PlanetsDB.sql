0fc069d1ee23256f338dfe77da771a00b48c16ff
Ben Arayathel <ben.arayathel@gmail.com>
2020-08-18 20:43:05 -0400

    SQL Basics

---
+++ Week-2/Day-2-BasicSQL/PlanetsDB.sql
@@ -0,0 +1,36 @@
+
+select * from planets;
+
+delete from planets;
+
+create table planets(
+--int, varchar, nvarchar, decimal, boolean, https://www.postgresql.org/docs/9.5/datatype.html
+-- column name, column type, constraints 
+	planet_id serial primary key,
+	planet_name varchar(30) not null,
+	planet_description VARCHAR(60) not null unique,
+	has_rings boolean not null,
+	number_of_moons smallint check(number_of_moons > -1)
+
+);
+
+
+insert into planets values(1, 'Mercury', 'Never too close', true, 0);
+
+insert into planets (planet_name, planet_description, has_rings, number_of_moons) values 
+	('Mercury', 'Never too close', true, 0),
+	('Venus', 'Getting a tad warm', false, 0),
+	('Earth', 'cradle of civilization', false, 1),
+	('Mars', 'Earth 2.0', false, 2),
+	('Jupiter', 'Big', true, 67),
+	('Saturn', 'very ringy', true, 82),
+	('Uranus', 'blue', true, 27),
+	('Neptune', 'getting nippy', true, 14),
+	('Pluto', 'you cannot get rid of me', false, 2);
+
+
+drop table planets;
+
+truncate table planets;
+
+alter table planets alter column number_of_moons set not null;
