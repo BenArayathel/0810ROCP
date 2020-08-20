14d88daa4b1352b5ce78c866d04f02d593dd6712
Ben Arayathel <ben.arayathel@gmail.com>
2020-08-20 12:03:13 -0400

    JDBC Demo

---
+++ Week-2/Day-4-JDBC/PlanetJDBC/src/main/java/com/revature/util/ConnectionFactory.java
@@ -0,0 +1,76 @@
+package com.revature.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	
+	/*
+	 * Java DataBase Connection
+	 * 
+	 * JDBC API handles database using Java, allows us to interact with 
+	 * our database through CRUD Methods
+	 * 
+	 * Core interfaces:
+	 * 		DriverManager
+	 * 		Connection 
+	 * 		Statement
+	 * 		PreparedStatement
+	 * 		CallableStatement
+	 * 		ResultSet
+	 * 
+	 */
+	
+	/*
+	 * Let's connect to our database: 
+	 * 1) URL endpoint
+	 * 2) username
+	 * 3) password
+	 * 4) driver (jar that implemets JDBC)
+	 * 
+	 */
+	
+	// "jdbc:postgresql://[HOST]/[DATABASE]
+	private static String url = "jdbc:postgresql://localhost/postgres";
+	private static String username = "postgres";
+	private static String password = "p4ssw0rd";
+	
+	public static Connection getConnection() {
+		
+		Connection conn = null;
+		
+		try {
+			conn = DriverManager.getConnection(url,username,password);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return conn;
+		
+	}
+	
+	//BEST PRACTICE CODE
+	public String random() {
+		int a = 1;
+		
+		String fruit;
+		
+//		if(a > 1) {
+//			return "Apples";
+//		} else {
+//			return "Kiwi";
+//		}
+		if(a > 1) {
+			fruit = "Apple";
+		} else {
+			fruit = "Kiwi";
+		}
+		
+		
+		return fruit;
+	}
+
+}
