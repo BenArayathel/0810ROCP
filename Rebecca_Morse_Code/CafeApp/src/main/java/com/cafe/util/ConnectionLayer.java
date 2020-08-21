1f3bb7fa3258b30841df8df96091af461e0576f0
Rebecca Morse <becca.morse@gmail.com>
2020-08-20 22:41:16 -0400

    add connection layer

--- Rebecca_Morse_Code/CafeApp/src/main/java/com/cafe/util/ConnectionLayer.java
+++ Rebecca_Morse_Code/CafeApp/src/main/java/com/cafe/util/ConnectionLayer.java
@@ -1,5 +1,26 @@
 package com.cafe.util;
 
-public class ConnectionLayer {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConnectionLayer {
+	private static final String URL = "jdbc:postgresql://localhost/postgres";
+	private static final String USERNAME = "postgres";
+	private static final String PASSWORD = "p";
+
+	public static Connection getConnection() {
+
+		Connection connection = null;
+
+		try {
+			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return connection;
+
+	}
 }
