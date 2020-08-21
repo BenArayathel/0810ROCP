c9c16b7e5efde4bece32f910487dd311b9cbaf86
Rebecca Morse <becca.morse@gmail.com>
2020-08-20 21:54:57 -0400

    refactor add menuservice

--- Rebecca_Morse_Code/CafeApp/src/main/java/com/cafe/driver/Driver.java
+++ Rebecca_Morse_Code/CafeApp/src/main/java/com/cafe/driver/Driver.java
@@ -8,14 +8,13 @@ import com.cafe.models.Customer;
 import com.cafe.models.FoodItem;
 import com.cafe.models.Menu;
 import com.cafe.models.Order;
+import com.cafe.service.MenuService;
 
 public class Driver {
 
 	public static final String WELCOME_MESSAGE = "Welcome to Cat Cafe! To place an order, Enter your Name:";
 	public static final String GREETING = "Hello, ";
 	public static final String ORDER_PROMPT = "What would you like to order?";
-	public static final String MENU_TITLE = "The Cat Cafe Menu:\n";
-	public static final String SELECT_PROMPT = "\nTo select an Item, enter the number in parentheses.";
 	public static final String QUANTITY_PROMPT_ONE = "How many ";
 	public static final String QUANTITY_PROMPT_TWO = "s would you like?";
 	public static final String DISPLAY_ORDER_TITLE = "Here is your order:";
@@ -31,6 +30,7 @@ public class Driver {
 
 	private static String action;
 	static int currentOrderNumber = 1;
+	private static MenuService menuService = new MenuService();
 
 	public static void main(String[] args) {
 
@@ -42,7 +42,7 @@ public class Driver {
 
 		communicate(GREETING + " " + customer.getName() + "!\n");
 
-		Menu menu = makeMenu();
+		Menu menu = menuService.makeMenu();
 
 		takeOrder(sc, customer, menu);
 		
@@ -76,8 +76,16 @@ public class Driver {
 		}
 	}
 
+	
+	private static Order startOrder() {
+		Map newOrderMap = new HashMap<FoodItem, Integer>();
+		Order order = new Order(currentOrderNumber, newOrderMap);
+		currentOrderNumber++;
+		return order;
+	}
+	
 	private static void presentMenuAndUpdateOrder(Scanner sc, Customer customer, Menu menu) {
-		presentMenu();
+		menuService.displayMenu(menu);
 		
 		int itemKey = Integer.parseInt(takeInput(sc));
 		switch(itemKey) {
@@ -102,11 +110,6 @@ public class Driver {
 
 	}
 
-	private static void checkout(Customer customer) {
-		Order order = customer.getOrder();
-		displayOrder(order);
-		communicate(THANK_YOU + customer.getName() + COME_AGAIN);
-	}
 	private static void displayOrder(Order order) {
 		communicate(DISPLAY_ORDER_TITLE);
 		for (Map.Entry<FoodItem, Integer> entry : order.getOrderContents().entrySet()) {
@@ -116,6 +119,12 @@ public class Driver {
 		}
 		float total = getOrderTotal(order);
 		communicate(ORDER_TOTAL_PREFIX + "$" + total);
+	}
+
+	private static void checkout(Customer customer) {
+		Order order = customer.getOrder();
+		displayOrder(order);
+		communicate(THANK_YOU + customer.getName() + COME_AGAIN);
 	}
 
 	private static float getOrderTotal(Order order) {
@@ -146,53 +155,9 @@ public class Driver {
 		Customer customer = new Customer(takeInput, startOrder());
 		return customer;
 	}
-
-	private static Order startOrder() {
-		Map newOrderMap = new HashMap<FoodItem, Integer>();
-		Order order = new Order(currentOrderNumber, newOrderMap);
-		currentOrderNumber++;
-		return order;
-	}
 	
-	private static void presentMenu() {
-		communicate(MENU_TITLE);
-		displayMenu(makeMenu());
-		communicate(SELECT_PROMPT);
-	}
 
-	public static Menu makeMenu() {
 
-		Map<Integer, FoodItem> menuContents = new HashMap<Integer, FoodItem>();
 
-		FoodItem water = new FoodItem("Water", "Beverage", "Liquid Refreshment at its' finest!", 0);
-		FoodItem lemonade = new FoodItem("Lemonade", "Beverage", "The tastiest Summertime Drink", 2);
-		FoodItem veggieBurger = new FoodItem("Veggie Burger", "Entree",
-				"Our House-made Veggie Burgers, served with hand-cut fries.", 10);
-		FoodItem pasta = new FoodItem("Pasta Supreme", "Entree", "House made pasta with the freshest Pesto around.", 7);
-		FoodItem asparagus = new FoodItem("Asparagus Spear", "Side", "Only the freshest!", 17);
-		FoodItem cococadoShake = new FoodItem("Cococado Vegan Milkshake", "Dessert",
-				"Made with rice dream ice cream, bananas, coconut and avocado.", 4);
-		FoodItem pie = new FoodItem("Pie", "Dessert", "Fresh seasonal pie.  Current variety: peach", 5);
-
-		menuContents.put(1, water);
-		menuContents.put(2, lemonade);
-		menuContents.put(3, veggieBurger);
-		menuContents.put(4, pasta);
-		menuContents.put(5, asparagus);
-		menuContents.put(6, cococadoShake);
-		menuContents.put(7, pie);
-
-		Menu menu = new Menu(menuContents);
-
-		return menu;
-	}
-
-	public static void displayMenu(Menu menu) {
-		for (Map.Entry<Integer, FoodItem> entry : menu.getMenu().entrySet()) {
-			System.out.println(
-					"(" + entry.getKey() + ") " + entry.getValue().getName() + 
-					" - " + entry.getValue().getDescription() + "	$" + entry.getValue().getCost());
-		}
-	}
 
 }
---
+++ Rebecca_Morse_Code/CafeApp/src/main/java/com/cafe/service/MenuService.java
@@ -0,0 +1,55 @@
+package com.cafe.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.cafe.models.FoodItem;
+import com.cafe.models.Menu;
+
+public class MenuService {
+	
+	public static final String MENU_TITLE = "The Cat Cafe Menu:\n";
+	public static final String SELECT_PROMPT = "\nTo select an Item, enter the number in parentheses.";
+
+	public static Menu makeMenu() {
+
+		Map<Integer, FoodItem> menuContents = new HashMap<Integer, FoodItem>();
+
+		FoodItem water = new FoodItem("Water", "Beverage", "Liquid Refreshment at its' finest!", 0);
+		FoodItem lemonade = new FoodItem("Lemonade", "Beverage", "The tastiest Summertime Drink", 2);
+		FoodItem veggieBurger = new FoodItem("Veggie Burger", "Entree",
+				"Our House-made Veggie Burgers, served with hand-cut fries.", 10);
+		FoodItem pasta = new FoodItem("Pasta Supreme", "Entree", "House made pasta with the freshest Pesto around.", 7);
+		FoodItem asparagus = new FoodItem("Asparagus Spear", "Side", "Only the freshest!", 17);
+		FoodItem cococadoShake = new FoodItem("Cococado Vegan Milkshake", "Dessert",
+				"Made with rice dream ice cream, bananas, coconut and avocado.", 4);
+		FoodItem pie = new FoodItem("Pie", "Dessert", "Fresh seasonal pie.  Current variety: peach", 5);
+
+		menuContents.put(1, water);
+		menuContents.put(2, lemonade);
+		menuContents.put(3, veggieBurger);
+		menuContents.put(4, pasta);
+		menuContents.put(5, asparagus);
+		menuContents.put(6, cococadoShake);
+		menuContents.put(7, pie);
+
+		Menu menu = new Menu(menuContents);
+
+		return menu;
+	}
+
+	public static void communicate(String string) {
+		System.out.println(string);
+	}
+	
+	public static void displayMenu(Menu menu) {
+		communicate(MENU_TITLE);
+
+		for (Map.Entry<Integer, FoodItem> entry : menu.getMenu().entrySet()) {
+			System.out.println("(" + entry.getKey() + ") " + entry.getValue().getName() + " - "
+					+ entry.getValue().getDescription() + "	$" + entry.getValue().getCost());
+		}
+		communicate(SELECT_PROMPT);
+	}
+
+}
