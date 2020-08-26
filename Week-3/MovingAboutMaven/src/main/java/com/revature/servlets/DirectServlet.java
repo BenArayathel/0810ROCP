1015c4e163d02a5784980c9eb79b1e935359ac09
Ben Arayathel <ben.arayathel@gmail.com>
2020-08-26 13:12:35 -0400

    direct response and indirect response, basic demos

---
+++ Week-3/MovingAboutMaven/src/main/java/com/revature/servlets/DirectServlet.java
@@ -0,0 +1,41 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class DirectServlet extends HttpServlet {
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Inside doGet Direct ");
+		
+		response.setContentType("text/html"); //This is unnecessary, because by default the content is text/html
+		
+		//For a direct response we can directly send a string that can be interpreted as an html file
+		//1) We have to instantiate a PrintWriter object from the response object
+		//2) We use the .write() method to send strings back. 
+		
+		PrintWriter pw = response.getWriter();
+		
+		String myPlanet = "pluto";
+		
+		pw.write("<h1>Welcome to my get method, I'm sending a direct response back</h1>");
+		pw.write("<h2> This is not really a planet " + myPlanet + " </h2>");
+		
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Inside post of direct");
+		doGet(request, response);
+		
+		
+	}
+
+}
---
+++ Week-3/MovingAboutMaven/src/main/java/com/revature/servlets/IndirectServlet.java
@@ -0,0 +1,80 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class IndirectServlet
+ */
+public class IndirectServlet extends HttpServlet {
+	
+	/*
+	 * There are 2 types of indirect responses 
+	 * 
+	 * Forwarding :
+	 * 		Allows you to navigate to another endpoint within your website (external websites will not work)
+	 * 		You only have to provide a portion of the url, not the whole website name.
+	 * 		NOTE : CLIENTS WILL NOT BE AWARE THEY HAVE BEEN FORWARDED
+	 * 		NOTE : There is a single request and response.
+	 * 
+	 * Redirecting 
+	 * 		Allows you to navigate to any resource on the web (that the client has access to)
+	 * 		You have to provide the WHOLE URI
+	 * 		NOTE : CLIENT WILL BE AWARE THEY HAVE BEEN REDIRECTED
+	 * 		NOTE : THERE ARE 2 REQUESTS AND RESPONSES, A REQUEST FROM THE CLIENT, A RESPONSE FROM THE 
+	 * 				SERVER TELLING THE CLIENT TO SEND ANOTHER REQUEST TO THE NEW URL AND A RESPONSE 
+	 * 				FROM THE NEW URL. 
+	 * 
+	 */
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*
+		 * REDIRECTING
+		 * 
+		 * 2 request and 2 responses. 
+		 * Client is aware of the external resource.
+		 * 
+		 */
+		
+		response.sendRedirect("https://www.google.com"); //WORKS WITH EXTERNAL RESOURCES 
+//		response.sendRedirect("http://localhost:9999/MovingAboutMaven/LandingPage.html"); //ENTIRE URI HAS TO BE SPECIFIED
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Inside Post method, forwarding.");
+		
+		/*
+		 * FORWARDING
+		 * 1 request and 1 response 
+		 * Client is UNAWARE of the new resource
+		 */
+		
+		/*
+		 * Forwarding requires the RequestDispatcher
+		 * 
+		 * The RequestDispatcher is an interface that defines an object to be able to handle requests 
+		 * 	and dispatch a resource back (servlet or html file) on the server.
+		 */
+		
+		
+		RequestDispatcher redir = request.getRequestDispatcher("/DirectServlet"); //THE ENTIRE URI  NO LONGER NEEDS TO BE SPECIFIED.
+	
+//		RequestDispatcher redir = request.getRequestDispatcher("/LandingPage.html"); // You can also specify html pages as well as servlets.
+		
+//		RequestDispatcher redir = request.getRequestDispatcher("/https://www.google.com"); //DOES NOT WORK WITH EXTERNAL RESOURCES
+		
+		redir.forward(request, response);
+
+	}
+
+}
